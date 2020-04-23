5984496687e0aecc04679d49fc7ce73c84255bc3
Julien Jakubowski <jjakubowski@octo.com>
2020-04-23 21:17:24 +0200

    characterization test

--- src/test/java/VideoStoreTest.java
+++ src/test/java/VideoStoreTest.java
@@ -6,7 +6,7 @@ public class VideoStoreTest {
     final Customer customer = new Customer("Fred");
 
     @Test
-    void oneRentalOneMovie() {
+    void oneRentalOneRegularMovie() {
         Movie movie = new Movie("2001: A Space Odyssey", Movie.REGULAR);
         int daysRented = 5;
         customer.addRental(new Rental(movie, daysRented));
@@ -15,5 +15,18 @@ public class VideoStoreTest {
                 "\t2001: A Space Odyssey\t6.5\n" +
                 "You owed 6.5\n" +
                 "You earned 1 frequent renter points\n");
+    }
+    
+    @Test
+    void twoRentalsOneMovie() {
+        Movie movie = new Movie("2001: A Space Odyssey", Movie.REGULAR);
+        customer.addRental(new Rental(movie, 5));
+        customer.addRental(new Rental(movie, 7));
+
+        assertThat(customer.statement()).isEqualTo("Rental Record for Fred\n" +
+                "\t2001: A Space Odyssey\t6.5\n" +
+                "\t2001: A Space Odyssey\t9.5\n" +
+                "You owed 16.0\n" +
+                "You earned 2 frequent renter points\n");
     }
 }
