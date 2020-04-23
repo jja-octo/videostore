b6e2cd6dd8142bbe7ddb88e3047167f22df47182
Julien Jakubowski <jjakubowski@octo.com>
2020-04-24 00:03:21 +0200

    refactor: pricing: move pricing computation to Pricing

--- src/main/java/videostore/Rental.java
+++ src/main/java/videostore/Rental.java
@@ -20,55 +20,18 @@ public class Rental
 		double amountToAdd = 0;
 		switch (getMovie ().getPricing()) {
 			case REGULAR:
-				amountToAdd = computeAmountRegular(daysRented);
+				amountToAdd = getMovie().getPricing().computeAmount(daysRented);
 				break;
 			case NEW_RELEASE:
-				amountToAdd = computeAmountNewRelease(daysRented);
+				amountToAdd = getMovie().getPricing().computeAmount(daysRented);
 				break;
 			case CHILDREN:
-				amountToAdd = computeAmountChildren(daysRented);
+				amountToAdd = getMovie().getPricing().computeAmount(daysRented);
 				break;
 		}
 		return amountToAdd;
 	}
 
-	private static double computeAmountChildren(int daysRented) {
-		double amountToAdd;
-		double childrenBasePrice = 1.5;
-		int childrenDaysPenalty = 3;
-		double childrenDailyPrice = 1.5;
-		amountToAdd = computeAmount(daysRented, childrenBasePrice, childrenDaysPenalty,
-				childrenDailyPrice);
-		return amountToAdd;
-	}
-
-	private static double computeAmountNewRelease(int daysRented) {
-		double amountToAdd;
-		double newReleaseBasePrice = 0;
-		int newReleaseDaysPenalty = 0;
-		int newReleaseDailyPrice = 3;
-		amountToAdd = computeAmount(daysRented, newReleaseBasePrice, newReleaseDaysPenalty,
-				newReleaseDailyPrice);
-		return amountToAdd;
-	}
-
-	private static double computeAmountRegular(int daysRented) {
-		double amountToAdd;
-		int regularBasePrice = 2;
-		int regularDaysPenalty = 2;
-		double regularDailyPrice = 1.5;
-		amountToAdd = computeAmount(daysRented, regularBasePrice, regularDaysPenalty,
-				regularDailyPrice);
-		return amountToAdd;
-	}
-
-	private static double computeAmount(int daysRented, double regularBasePrice, int regularDaysPenalty, double regularDailyPrice) {
-			double regularAmountToAdd = regularBasePrice;
-			if (daysRented > regularDaysPenalty) {
-				regularAmountToAdd += (daysRented - regularDaysPenalty) * regularDailyPrice;
-			}
-			return regularAmountToAdd;
-		}
 	
 	private Movie movie;
 	private int daysRented;
