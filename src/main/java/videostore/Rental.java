da922e0e26540ccbee31495b2e969d9cf7d199a4
Julien Jakubowski <jjakubowski@octo.com>
2020-04-24 00:04:15 +0200

    refactor: pricing: remove redundancy

--- src/main/java/videostore/Rental.java
+++ src/main/java/videostore/Rental.java
@@ -16,22 +16,8 @@ public class Rental
 	}
 	
 	public double amount() {
-		int daysRented = getDaysRented();
-		double amountToAdd = 0;
-		switch (getMovie ().getPricing()) {
-			case REGULAR:
-				amountToAdd = getMovie().getPricing().computeAmount(daysRented);
-				break;
-			case NEW_RELEASE:
-				amountToAdd = getMovie().getPricing().computeAmount(daysRented);
-				break;
-			case CHILDREN:
-				amountToAdd = getMovie().getPricing().computeAmount(daysRented);
-				break;
-		}
-		return amountToAdd;
+		return getMovie().getPricing().computeAmount(getDaysRented());
 	}
-
 	
 	private Movie movie;
 	private int daysRented;
