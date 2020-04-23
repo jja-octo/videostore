e98df1e4a1d55d241158efb646f4dc6744621e8d
Julien Jakubowski <jjakubowski@octo.com>
2020-04-23 19:37:13 +0200

    first characterization test

--- src/test/java/VideoStoreTest.java
+++ src/test/java/VideoStoreTest.java
@@ -1,11 +1,19 @@
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class VideoStoreTest {
-    final Customer customer = new Customer("Fred");;
+    final Customer customer = new Customer("Fred");
 
     @Test
-    @Disabled
-    void test() {
+    void oneRentalOneMovie() {
+        Movie movie = new Movie("2001: A Space Odyssey", Movie.REGULAR);
+        int daysRented = 5;
+        customer.addRental(new Rental(movie, daysRented));
+
+        assertThat(customer.statement()).isEqualTo("Rental Record for Fred\n" +
+                "\t2001: A Space Odyssey\t6.5\n" +
+                "You owed 6.5\n" +
+                "You earned 1 frequent renter points\n");
     }
 }
