e1ccf9ab0fd84441ce44d9066f568a78d1e0469d
Julien Jakubowski <jjakubowski@octo.com>
2020-04-23 22:41:12 +0200

    move code to a package to make PIT work

--- src/test/java/videostore/GoldenMasterGenerator.java
+++ src/test/java/videostore/GoldenMasterGenerator.java
@@ -1,3 +1,5 @@
+package videostore;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
---
+++ src/test/java/videostore/VideoStoreTest.java
@@ -0,0 +1,34 @@
+package videostore;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class VideoStoreTest {
+    final Customer customer = new Customer("Fred");
+
+    @Test
+    void oneRentalOneRegularMovie() {
+        Movie movie = new Movie("2001: A Space Odyssey", Movie.REGULAR);
+        int daysRented = 5;
+        customer.addRental(new Rental(movie, daysRented));
+
+        assertThat(customer.statement()).isEqualTo("Rental Record for Fred\n" +
+                "\t2001: A Space Odyssey\t6.5\n" +
+                "You owed 6.5\n" +
+                "You earned 1 frequent renter points\n");
+    }
+
+    @Test
+    void twoRentalsOneMovie() {
+        Movie movie = new Movie("2001: A Space Odyssey", Movie.REGULAR);
+        customer.addRental(new Rental(movie, 5));
+        customer.addRental(new Rental(movie, 7));
+
+        assertThat(customer.statement()).isEqualTo("Rental Record for Fred\n" +
+                "\t2001: A Space Odyssey\t6.5\n" +
+                "\t2001: A Space Odyssey\t9.5\n" +
+                "You owed 16.0\n" +
+                "You earned 2 frequent renter points\n");
+    }
+}
