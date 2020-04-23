8536f5b5f9e979429edbc243180c80ff0751f887
Julien Jakubowski <jjakubowski@octo.com>
2020-04-23 19:30:17 +0200

    mavenification, Java 13, JUnit 5, AssertJ, legacy test removal, CRLF->LF

---
+++ src/test/java/VideoStoreTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class VideoStoreTest {
+    final Customer customer = new Customer("Fred");;
+
+    @Test
+    @Disabled
+    void test() {
+    }
+}
