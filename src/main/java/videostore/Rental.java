3774384059ce8f91b613a2bb8ed246cc8f437aa9
Julien Jakubowski <jjakubowski@octo.com>
2020-04-24 00:16:58 +0200

    refactor: rental optimisation: compute amount only once on object creation

--- src/main/java/videostore/Rental.java
+++ src/main/java/videostore/Rental.java
@@ -5,20 +5,22 @@ public class Rental
 	public Rental (Movie movie, int daysRented) {
 		this.movie 		= movie;
 		this.daysRented = daysRented;
+		this.amount = getMovie().getPricing().rentalAmount(getDaysRented());
 	}
-	
+
 	public int getDaysRented () {
 		return daysRented;
 	}
-	
+
 	public Movie getMovie () {
 		return movie;
 	}
-	
+
 	public double amount() {
-		return getMovie().getPricing().rentalAmount(getDaysRented());
+		return amount;
 	}
-	
-	private Movie movie;
-	private int daysRented;
+
+	private final double amount;
+	private final Movie movie;
+	private final int daysRented;
 }
